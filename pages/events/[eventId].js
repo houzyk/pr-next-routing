4a589ce4aaaf9cf2fa4db106dc23d54ed452c46b
houzyk <houzairmk@icloud.com>
2022-01-21 09:12:33 +0400

    ADD event components + css

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,6 +1,37 @@
+// data
+import { getEventById } from "../../data";
+
+// next
+import { useRouter } from "next/router";
+
+// component
+import EventSummary from "../../components/event-detail/event-summary";
+import EventContent from "../../components/event-detail/event-content";
+import EventLogistics from "../../components/event-detail/event-logistics";
+
 export default function EventShowPage () {
+  const router = useRouter();
+
+  const event = getEventById(router.query.eventId);
+
+  if (!event) {
+    return (
+      <p>No Event Found</p>
+    );
+  }
+
   return (
     <>
+      <EventSummary title={event.title} />
+      <EventLogistics 
+        date={event.date} 
+        address={event.location} 
+        image={event.image} 
+        imageAlt={event.title}
+      />
+      <EventContent>
+        <p>{event.description}</p>
+      </EventContent>
     </>
   );
 }
