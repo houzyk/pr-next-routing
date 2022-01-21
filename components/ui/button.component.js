f4fb7dd010623c67d4296db7eaec9838a193b73d
houzyk <houzairmk@icloud.com>
2022-01-21 13:32:55 +0400

    ADD event search component + css

--- components/ui/button.component.js
+++ components/ui/button.component.js
@@ -4,13 +4,24 @@ import Link from "next/link";
 // css
 import styles from './button.styles.module.css'
 
-function Button({ children, link }) {
+function Button({ children, link, onClick }) {
+
+  if (link) {
+    return (
+      <Link href={link}>
+        <a className={styles.btn}>
+          {children}
+        </a>
+      </Link>
+    );
+  }  
   return (
-    <Link href={link}>
-      <a className={styles.btn}>
-        {children}
-      </a>
-    </Link>
+    <button 
+      className={styles.btn}
+      onClick={onClick}
+    >
+      {children}
+    </button>
   );
 }
 
--- pages/events/index.js
+++ pages/events/index.js
@@ -3,8 +3,14 @@ import { getAllEvents } from "../../data";
 
 // components
 import EventList from "../../components/events/event-list.component";
+import EventSearch from "../../components/events/event-search.component";
 
 export default function EventIndexPage () {
   const events = getAllEvents();
-  return <EventList events={events} />
+  return (
+    <>
+      <EventSearch />
+      <EventList events={events} />
+    </>
+  );
 }
